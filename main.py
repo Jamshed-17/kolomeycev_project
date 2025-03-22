f750932e8cf53731c41ece2a4afbdbf57dd5e7bf
jamshed <jamshhed@vk.ru>
2025-03-22 23:13:24 +0500

    try to fix

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import requests
 import telebot
-
-import json #json )
-import pprint #красивый вывод json
+import time
+import json
 from bs4 import BeautifulSoup as BS
 
 bot = telebot.TeleBot('8034462858:AAFODzmWHFofakAomMOGEilsyu06XW1sCbs')
@@ -11,40 +10,40 @@ start_price = ""
 start_price1 = ""
 
 while True:
-    #link = "https://steamcommunity.com/market/listings/730/M4A4%20%7C%20The%20Battlestar%20(Field-Tested)?l=russian" #сама пагадже
-    #maket = "https://steamcommunity.com/market/itemordershistogram?country=RU&language=russian&currency=1&item_nameid="
-    item_id = "139654846"
-    currency = 37    # 1-usd; 2-франки;3 -евро;5-рубли
-    link = f"https://steamcommunity.com/market/itemordershistogram?country=RU&language=russian&currency={currency}&item_nameid={item_id}" #api link for request
+    try:
+        item_id = "139654846"
+        currency = 37
+        link = f"https://steamcommunity.com/market/itemordershistogram?country=RU&language=russian&currency={currency}&item_nameid={item_id}"
 
-    req = requests.get(link) #делаем get
+        req = requests.get(link)
+        req.raise_for_status()  # Проверка на ошибки HTTP
 
-    # price = soup.select(".market_commodity_orders_header_promote") #здесь только ради тега
-    js = json.loads(req.content) #загружаем json
-    searched_tag = js["sell_order_summary"] #берем нужное
-    searched_tag1 = js["buy_order_summary"] #берем нужное
-    addition_soup = BS(searched_tag, 'html.parser') #это вообще только ради того, чтобы взять только текст
-    addition_soup1 = BS(searched_tag1, 'html.parser') #это вообще только ради того, чтобы взять только текст
+        js = json.loads(req.content)
+        searched_tag = js["sell_order_summary"]
+        searched_tag1 = js["buy_order_summary"]
+        addition_soup = BS(searched_tag, 'html.parser')
+        addition_soup1 = BS(searched_tag1, 'html.parser')
 
+        new_price = addition_soup1.text.split(':')[-1]
+        new_price1 = addition_soup.text.split(':')[-1]
 
-    if start_price == (addition_soup1.text.split(':')[-1]) and start_price1 == (addition_soup.text.split(':')[-1]):
-        pass
-    else:
+        if start_price != new_price or start_price1 != new_price1:
+            finishText = (
+                "Название: M4A4 | Звездный крейсер\n"
+                + "Износ: После полевых испытаний\n"
+                + addition_soup1.text.split('Начальная цена')[-2]
+                + "\nНачальная цена: " + new_price
+                + "\nМаксимальная цена: " + new_price1
+            )
 
+            start_price = new_price
+            start_price1 = new_price1
+            bot.send_message(2008381570, finishText)
 
-        finishText = ("Название: M4A4 | Звездный крейсер\n"
-                      + "Износ: После полевых испытаний\n"
-                      + addition_soup1.text.split('Начальная цена')[-2]
-                      + "\nНачальная цена: "
-                      + addition_soup1.text.split(':')[-1]
-                      + "\nМаксимальная цена: "
-                      + addition_soup.text.split(':')[-1])
+        else:
+            pass
 
-        start_price = addition_soup1.text.split(':')[-1]
-        start_price1 = addition_soup.text.split(':')[-1]
+    except Exception as e:
+        bot.send_message(2008381570, f"Ошибка - {e}")
 
-        bot.send_message(2008381570, finishText)
-        print(start_price)
-        print(start_price1)
-        print()
     time.sleep(60)
